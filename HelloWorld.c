8f5bf0588a4c4877c62db26137b6eabe90d73f63
Maximilian Lee <maxlee695@gmail.com>
2025-11-04 17:28:55 +0000

    Initialised repo

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main (void)
+{
+    /*exaple comments*/
+    printf("Hello World\n");
+    printf("Hello World 2 electric boogaloo\n");
+
+    return 0;
+}
